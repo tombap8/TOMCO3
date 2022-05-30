2b608f8c6c194a1c1a0caa94435c8e7efd3e427d
Tom B. Parker <tombap8@hotmail.com>
2022-05-30 17:19:33 +0900

    ss

--- 001.HTML학습/07.CGV_PJ/003.site_v1/js/main.js
+++ 001.HTML학습/07.CGV_PJ/003.site_v1/js/main.js
@@ -9,5 +9,14 @@ function chgMV(vid){ // vid - 동영상아이디
     // 1. 함수호출확인+전달값 확인
     console.log("바꿔!",vid);
 
+    // 2. 변경대상: #screen iframe
+    var tg = document.querySelector("#screen iframe");
+
+    // 3. 변경내용: src속성 재설정하기
+    // 선택요소.src = 변경할값
+    tg.src = `https://www.youtube.com/embed/
+    ${vid}?autoplay=1&loop=1
+    &playlist=${vid}&playsinline=1`;
+
 } /////////////// chgMV 함수 //////////////////
 ///////////////////////////////////////////////
