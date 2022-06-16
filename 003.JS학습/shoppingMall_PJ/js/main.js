b6a55e7125bb238f70ec313af5d67bb598bb377a
Tom B. Parker <tombap8@hotmail.com>
2022-06-16 10:51:47 +0900

    Update main.js

--- 003.JS학습/shoppingMall_PJ/js/main.js
+++ 003.JS학습/shoppingMall_PJ/js/main.js
@@ -79,12 +79,15 @@ function loadFn() {
             // console.log("광클막기:",prot);
 
             ///////// 광클금지 /////////
-            if(prot) return; //돌아가! 
+            if (prot) return; //돌아가! 
             prot = 1; //잠금!
-            setTimeout(()=>prot=0,410);
+            setTimeout(() => prot = 0, 410);
             // 타임아웃으로 슬라이드이동 후
             // 잠금설정을 prot=0으로 해제
             ////////////////////////////
+
+            // 인터발지우기 함수호출!
+            clearAuto();
 
             // 1. 오른쪽버튼 여부
             let isR = x.classList.contains("ab2");
@@ -94,29 +97,9 @@ function loadFn() {
 
             // 2. 오른쪽/왼쪽버튼 분기하기
             if (isR) { // 오른쪽버튼 ///
-                // 1. 슬라이드 left:-100% + 트랜지션
-                slide.style.left = "-100%";
-                slide.style.transition =
-                    "left .4s ease-out";
 
-                // 이동후 실행 -> 이동시간은 0.4초
-                // setTimeout(함수,시간) -> 일정시간후 한번실행!
-                setTimeout(() => {
-
-                    // 2. 첫번째 li를 맨뒤로 이동
-                    // 첫번째 li
-                    let fli = slide
-                        .querySelectorAll("li")[0];
-                    // 맨뒤로 이동
-                    slide.appendChild(fli);
-
-                    // 3. 동시에 left값 0
-                    slide.style.left = "0";
-                    // 이때 트랜지션 해제!
-                    slide.style.transition = "none";
-
-                }, 400); //// 타임아웃 ///////
-
+                // 오른쪽 이동함수 호출!
+                goRight();
 
             } //////////// if //////////
             else { // 왼쪽버튼 ///////
@@ -144,39 +127,121 @@ function loadFn() {
                         "left .4s ease-out";
                 }, 10); ///// 타임아웃 ////
 
+                // 4. 블릿작동함수 호출!
+                goIndic(0);
+                // 파라미터값으로 0 -> 왼쪽은 0
+
             } //////////// else /////////
-
-            // 4. 공통기능 : 블릿변경하기
-            // 블릿 class="on" 지우기 초기화
-            for (let x of indic)
-                x.classList.remove("on");
-
-            // 첫번째 슬라이드 "data-seq"값을
-            // 읽어와서 블릿순번에 적용하기!
-            // 주의: 오른쪽버튼은 [1], 왼쪽버튼은 [0]
-            // 즉, 오른쪽은 두번째li, 왼쪽은 첫번째li
-            // 오른쪽 버튼이면 isR 변수값이 true
-            // isR?1:0 -> 비?집:놀이동산 (조건연산자===삼항연산자)
-            // isR값이 true이면 1, 아니면 0
-            let fseq = slide
-                .querySelectorAll("li")[isR?1:0]
-                .getAttribute("data-seq");
-            // getAttribute(속성명) -> 속성값을 읽어옴!
-
-            // console.log("오른쪽/왼쪽다름:",isR?1:0);
-
-            // console.log("fseq값:",fseq);
-            // console.log("fseq값의 형:",typeof fseq);
-
-            indic[fseq].classList.add("on");
-            // 원래는 fseq는 숫자값인데 숫자형이어야함
-            // 그래서 Number(fseq)로 형변환 해야했었음...
-            // 그런데 요즘 브라우저에서는 이런부분을 
-            // 형변환하지 않아도 숫자이면 숫자형으로 변환해줌~!
 
         }; /////// click ///////
 
     } /////////// for of //////////////
+
+    /***************************************** 
+        함수명: goIndic
+        기능: 블릿 표시자 작동하기
+    *****************************************/
+    const goIndic = 헐 => { // 헐 : 오른쪽 1, 왼쪽 0
+
+        // [ 공통기능 : 블릿변경하기 ]
+
+        // 블릿 class="on" 지우기 초기화
+        for (let x of indic)
+            x.classList.remove("on");
+
+        // 첫번째 슬라이드 "data-seq"값을
+        // 읽어와서 블릿순번에 적용하기!
+        // 주의: 오른쪽버튼은 [1], 왼쪽버튼은 [0]
+        // 즉, 오른쪽은 두번째li, 왼쪽은 첫번째li
+        // 오른쪽 버튼이면 isR 변수값이 true
+        // isR?1:0 -> 비?집:놀이동산 (조건연산자===삼항연산자)
+        // isR값이 true이면 1, 아니면 0
+        let fseq = slide
+            .querySelectorAll("li")[헐 ? 1 : 0]
+            .getAttribute("data-seq");
+        // getAttribute(속성명) -> 속성값을 읽어옴!
+
+        // console.log("오른쪽/왼쪽다름:",isR?1:0);
+
+        // console.log("fseq값:",fseq);
+        // console.log("fseq값의 형:",typeof fseq);
+
+        indic[fseq].classList.add("on");
+        // 원래는 fseq는 숫자값인데 숫자형이어야함
+        // 그래서 Number(fseq)로 형변환 해야했었음...
+        // 그런데 요즘 브라우저에서는 이런부분을 
+        // 형변환하지 않아도 숫자이면 숫자형으로 변환해줌~!
+
+    }; /////////// goIndic 함수 ////////////////
+    ////////////////////////////////////////////
+
+    /***************************************** 
+        함수명: goRight
+        기능: 오른쪽 슬라이드 이동기능
+    *****************************************/
+    const goRight = () => {
+
+        // 1. 슬라이드 left:-100% + 트랜지션
+        slide.style.left = "-100%";
+        slide.style.transition =
+            "left .4s ease-out";
+
+        // 이동후 실행 -> 이동시간은 0.4초
+        // setTimeout(함수,시간) -> 일정시간후 한번실행!
+        setTimeout(() => {
+
+            // 2. 첫번째 li를 맨뒤로 이동
+            // 첫번째 li
+            let fli = slide
+                .querySelectorAll("li")[0];
+            // 맨뒤로 이동
+            slide.appendChild(fli);
+
+            // 3. 동시에 left값 0
+            slide.style.left = "0";
+            // 이때 트랜지션 해제!
+            slide.style.transition = "none";
+
+        }, 400); //// 타임아웃 ///////
+
+        // 3. 블릿작동함수 호출!
+        goIndic(1); // 오른쪽은 전달값 1
+
+    }; //////////// goRight 함수 ///////////////
+    ////////////////////////////////////////////
+
+    // 인터발용변수
+    let autoI;
+
+    // 인터발 셋팅 함수 //////////////
+    const autoCall = () =>
+        autoI = setInterval(goRight, 2000);
+
+    // 인터발 셋팅 함수 최초호출!
+    autoCall();
+
+    // 타임아웃용 변수
+    let autoT;
+
+    // 인터발 지우기 함수 /////////////
+    const clearAuto = () => {
+        console.log("인터발지움!");
+
+        // 인터발지우기
+        clearInterval(autoI);
+
+        // 타임아웃지우기(실행쓰나미방지!)
+        clearTimeout(autoT);
+
+        // 일정시간후 인터발셋팅(4초후)
+        autoT = setTimeout(autoCall, 4000);
+        // 매번 타임아웃을 변수에 담고 먼저 지우기 때문에
+        // 최종적으로 남는 타임아웃은 하나뿐이다!
+        // 따라서 타임아웃 실행 쓰나미가 발생하지 않는다!
+
+    }; ///////// clearAuto 함수 ///////
+
+
 
 } //////////////// loadFn 함수 ///////////////
 /////////////////////////////////////////////
