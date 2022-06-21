28cce2241b7e89ea88552781d675caad41a75a66
Tom B. Parker <tombap8@hotmail.com>
2022-06-21 12:15:56 +0900

    ss

--- 001.HTML학습/08.VOGUE_PJ/002.site/js/linksys.js
+++ 001.HTML학습/08.VOGUE_PJ/002.site/js/linksys.js
@@ -4,11 +4,31 @@ $(()=>{ //////////// jQB ////////////////////
 
     // GNB 링크 셋팅 //////////////////
     // 대상: .gnb a
-    $('.gnb a').click(function(){
+    $('.gnb a').click(function(e){ // e-이벤트 전달변수
+
+        // a요소 기본이동막기
+        // (제이쿼리방법:코딩순서무관)
+        // e라는 이벤트 전달변수를 사용하여 셋팅!
+        e.preventDefault();
 
         // 클릭된 a요소 글자읽기
-        let txt = $(this).text();
+        let txt = $(this).text().trim().toLowerCase();
+        // trim() 앞뒤공백제거
+        // toLowerCase() 소문자변환
+        // 참고) toUpperCase() 대문자변환
         console.log(txt);
+
+        // 서브페이지 이동하기
+        if(txt!=="search")
+            location.href = "category.html?cat="+txt;
+        // 카테고리를 구분하기위한 파라미터 키=값 쌍을 보냄
+        // cat=카테고리명
+        // 이것을 받아서 페이지 셋업을 한다!
+        // 이렇게 데이터를 url로 전달하는 방식을 GET방식이라고 함!
+
+        // a요소 기본이동막기
+        // (JS 기본방법 - 하단에 코딩)
+        // return false;
 
     }); ///////////// click ////////////
 
