58c5492129bb3094a71f3b4786650fec3a9b323e
Tom B. Parker <tombap8@hotmail.com>
2022-06-22 12:07:26 +0900

    Update member.js

--- 001.HTML학습/08.VOGUE_PJ/002.site/js/member.js
+++ 001.HTML학습/08.VOGUE_PJ/002.site/js/member.js
@@ -10,8 +10,23 @@ $(()=>{/////////////// jQB /////////////////////
         - 제이쿼리 이벤트 메서드: blur()
         - 이벤트 대상:
 
-        
+        -> id가 "email2"가 아니고 
+            class가 "search"가 아닌 
+            type이 "text"인 입력요소 input
+
+        >>> 제이쿼리 선택 표현법:
+        input[type=text][id!=email2][class!=search]
+        >>> 대괄호[]는 속성선택, != 같지않다(제이쿼리전용)
+
+        -> type이 "password"인 입력요소 input
+
+        >>> 제이쿼리 선택 표현법:
+        input[type=password]
+
     ******************************************/
+   $(`input[type=text][id!=email2][class!=search],
+   input[type=password]`)
+   .blur()
 
 
 
